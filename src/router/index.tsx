9476e19618d0892009a16f4dba9924a6bbcd5030
smolskig <contatosmolski@gmail.com>
2023-08-28 18:16:41 -0300

    fixing route name

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -6,13 +6,13 @@ import {
 
 import App from "../App";
 import Home from "../pages/Home";
-import JobPage from "../pages/JobItem";
+import JobDetails from "../pages/JobDetails";
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route element={<App />}>
       <Route path="/" element={<Home />} />
-      <Route path=":id" element={<JobPage />}></Route>
+      <Route path=":id" element={<JobDetails />}></Route>
     </Route>
   )
 );
